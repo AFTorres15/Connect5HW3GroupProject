3a198ae4ccffad891953baffb0d83b2d51a99fe6
AFTorres15 <torreand15@gmail.com>
2018-07-02 22:09:50 -0600

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,19 @@
+package cs3331;
+
+public class Player {
+    private int playerName;
+    private char symbol;
+    private boolean isReal;
+    public Player(int currPlayer,char symbol){
+        this.playerName=currPlayer;
+        this.symbol=symbol;
+        isReal=false;
+    }
+    public char getSymbol(){
+        return symbol;
+    }
+    public int getPlayerName(){
+        return playerName;
+    }
+    //Eventually we will implement the AI stuff here or related to here that is why we have the irReal
+}
